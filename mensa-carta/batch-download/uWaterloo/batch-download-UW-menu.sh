460de9805485cc1f133dbdd2018fbf784a5c5597
Anthony Wang <anthony.wang@rwth-aachen.de>
2018-03-25 14:45:47 +0200

    - Cleaned up scripts - Changed file naming scheme in scripts

--- mensa-carta/batch-download/uWaterloo/batch-download-UW-menu.sh
+++ mensa-carta/batch-download/uWaterloo/batch-download-UW-menu.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-#API documentation: https://github.com/uWaterloo/api-documentation/blob/master/README.md
+# API documentation: https://github.com/uWaterloo/api-documentation/blob/master/README.md
 echo "Please input your API Key: "
 read UW_API_KEY
+
+# Iterate over year
 for i in $(seq -w 2007 2008); do
+	# Iterate over week
 	for j in $(seq -w 1 52); do
-    	printf "%s-w%smenu.json https://api.uwaterloo.ca/v2/foodservices/%s/%s/menu.json?key=%s\n"\
+    	printf "%s-w%s-menu.json https://api.uwaterloo.ca/v2/foodservices/%s/%s/menu.json?key=%s\n"\
     	$i $j $i $j $UW_API_KEY
 	done 
 done | xargs -t -n2 -P4 wget -O
--- mensa-carta/batch-download/uWaterloo/batch-download-UW-products.sh
+++ mensa-carta/batch-download/uWaterloo/batch-download-UW-products.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-#API documentation: https://github.com/uWaterloo/api-documentation/blob/master/README.md
+# API documentation: https://github.com/uWaterloo/api-documentation/blob/master/README.md
 echo "Please input your API Key: "
 read UW_API_KEY
-product_ids=( 1966 1512 742 790 ) #space-deliminated array
+
+# Space-deliminated array of product IDs -> get the IDs from parsing the menus
+product_ids=( 1966 1512 742 790 )
+echo "Product IDs: "
 echo product_ids
-for i in "${product_ids[@]}"; do #Need to iterate over product IDs -> get the IDs from parsing the menus
+
+# Iterate over the product IDs
+for i in "${product_ids[@]}"; do
     	printf "product%s.json https://api.uwaterloo.ca/v2/foodservices/products/%s.json?key=%s\n"\
     	$i $i $UW_API_KEY
 done | xargs -t -n2 -P4 wget -O
