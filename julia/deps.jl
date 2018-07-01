009247813ba3615118483e049ba1a039151fcc13
Anthony Wang-Wonton-Soup <anthony.wang@rwth-aachen.de>
2018-07-01 20:48:36 +0200

    Use deps

--- julia/deps.jl
+++ julia/deps.jl
@@ -2,4 +2,7 @@ println("Loading deps")
 
 using JSON
 using DataFrames
+# represent the data more efficiently by replacing the strings with indices into a small pool of identical levels
+using CategoricalArrays, Missings
+using Query
 using Plots
