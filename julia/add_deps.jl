93afc9fe197f16ca156adb1265824801ecc7f138
Anthony Wang-Wonton-Soup <anthony.wang@rwth-aachen.de>
2018-07-14 12:28:08 +0200

    Broadcast add dependencies

--- julia/add_deps.jl
+++ julia/add_deps.jl
@@ -1,6 +1,8 @@
 println("Adding packages for deps")
 
-Pkg.add("JSON")
-Pkg.add("CategoricalArrays")
-Pkg.add("Missings")
-Pkg.add("Query")
+# Pkg.add("JSON")
+# Pkg.add("CategoricalArrays")
+# Pkg.add("Missings")
+# Pkg.add("Query")
+
+Pkg.add.(["JSON", "CategoricalArrays", "Missings", "Query"])
